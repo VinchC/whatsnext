88aee933f269384589276f459fbb6b506fcf5d1f
VinchC <vinch.cm@gmail.com>
2024-09-24 11:31:06 +0200

    route update /lps/:id added

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,18 @@ import { lps } from "./lps";
 
 const app = express();
 
-const port = process.env.REACT_APP_SERVER_PORT;
+const port = process.env.REACT_APP_SERVER_PORT; // gets the port defined in a secret variable in the .env file
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
+app.use(express.json()); //allows implementation of the post route
+
+// a route should always start with a "/"...
+
 app.get("/", (req, res) => {
-  res.send("HELLO");
+  res.send("HELLO"); // the send function displays text on the page
 });
 
 app.get("/lps", (req, res) => {
@@ -23,8 +27,8 @@ app.get("/lps/:id", (req, res) => {
   const id = parseInt(req.params.id); // gets the URL parameter related to the item in particular
   const lp = lps.find((lp) => lp.id === id); // gets the item which id is the same as the one in the URL
 
+  // if item doesn't exist
   if (!lp) {
-    // if item doesn't exist
     res.sendStatus(404);
   }
 
@@ -35,8 +39,8 @@ app.delete("/lps/:id", (req, res) => {
   const id = parseInt(req.params.id); // gets the URL parameter related to the item in particular
   const lpIndex = lps.findIndex((lp) => lp.id === id); // gets the index of an item which is the same as the one in the URL
 
+  // if item with this index doesn't exist
   if (lpIndex === -1) {
-    // if item with this index doesn't exist
     res.sendStatus(404);
   }
 
@@ -45,12 +49,26 @@ app.delete("/lps/:id", (req, res) => {
   res.json({ lp }); // returns the deleted item
 });
 
-app.use(express.json()); //allows implementation of the post route
-
-// route should always start with a "/"...
 app.post("/lps", (req, res) => {
   const lp = req.body;
 
   lps.push(lp);
   res.status(201).json({ lp });
+});
+
+app.put("/lps/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const lpIndex = lps.findIndex((lp) => lp.id === id);
+
+  if (lpIndex === -1) {
+    res.sendStatus(404);
+  }
+
+  const lp = lps[lpIndex];
+  const newData = req.body;
+
+  const updatedData = { ...lp, ...newData };
+  lps.splice(lpIndex, 1, updatedData);
+
+  res.status(204).json({ lp: updatedData });
 });
