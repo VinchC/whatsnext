e609cf9a75f694792fe6bdaa34ea1674dd57ab2f
VinchC <vinch.cm@gmail.com>
2024-09-24 08:53:08 +0200

    route post /lps added

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ const app = express();
 
 const port = process.env.REACT_APP_SERVER_PORT;
 
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
 app.get("/", (req, res) => {
   res.send("HELLO");
 });
@@ -15,6 +19,12 @@ app.get("/lps", (req, res) => {
   res.json({ lps }); // adding or removing brackets change the data displayed, including the name of the variable as a data if present
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+app.use(express.json()); //allows implementation of the post route
+
+// route should always start with a "/"...
+app.post("/lps", (req, res) => {
+  const lp = req.body;
+
+  lps.push(lp);
+  res.json({ lp });
 });
