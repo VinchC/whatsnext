562ffd4d073386b272df07e6a0309b97455983ab
VinchC <vinch.cm@gmail.com>
2024-09-24 10:56:23 +0200

    delete /lps/:id route added

--- src/index.ts
+++ src/index.ts
@@ -20,12 +20,29 @@ app.get("/lps", (req, res) => {
 });
 
 app.get("/lps/:id", (req, res) => {
-  const id = parseInt(req.params.id); // get the URL parameter related to the item in particular
-  const lp = lps.find((lp) => lp.id === id); // get the items which id is the same as the one in the URL
+  const id = parseInt(req.params.id); // gets the URL parameter related to the item in particular
+  const lp = lps.find((lp) => lp.id === id); // gets the item which id is the same as the one in the URL
+
   if (!lp) {
+    // if item doesn't exist
     res.sendStatus(404);
   }
+
   res.json({ lp });
+});
+
+app.delete("/lps/:id", (req, res) => {
+  const id = parseInt(req.params.id); // gets the URL parameter related to the item in particular
+  const lpIndex = lps.findIndex((lp) => lp.id === id); // gets the index of an item which is the same as the one in the URL
+
+  if (lpIndex === -1) {
+    // if item with this index doesn't exist
+    res.sendStatus(404);
+  }
+
+  const lp = lps[lpIndex]; // gets the item which index corresponds to the params of the URL
+  lps.splice(lpIndex, 1); // removes 1 item starting at the index corresponding to the params of the URL
+  res.json({ lp }); // returns the deleted item
 });
 
 app.use(express.json()); //allows implementation of the post route
