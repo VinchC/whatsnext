0be060423c570f417a30af925d2af95454aa8fa8
VinchC <vinch.cm@gmail.com>
2024-10-04 09:13:07 +0200

    constant variables created and exported in styles.components

---
+++ front/src/styles/constants.ts
@@ -0,0 +1,14 @@
+import { css } from "styled-components";
+
+export const PRIMARY_COLOR = "#a9a9a9"
+
+export const SECONDARY_COLOR = "#darkblue";
+
+export const TERTIARY_COLOR = "#f0f0f0";
+
+export const BORDER_COLOR = "black";
+
+export const TRANSPARENT_BACKGROUND = css`
+  background-color: black;
+  opacity: 0.5;
+`;
