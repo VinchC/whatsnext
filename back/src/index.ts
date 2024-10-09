0dca7ef155ae3fff270f66dd166057955af6106f
VinchC <vinch.cm@gmail.com>
2024-10-09 10:50:44 +0200

    Express server and routes removed

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,10 +1,8 @@
-import express from "express";
 require("dotenv").config();
 import { DataSource } from "typeorm";
 import "reflect-metadata";
 
 import Lp from "./entities/Lp";
-import { isError } from "./utils";
 import Category from "./entities/Category";
 import Tag from "./entities/Tag";
 
@@ -15,22 +13,7 @@ import { buildSchema } from "type-graphql";
 import { LpResolver } from "./resolvers/LpResolver";
 import { TagResolver } from "./resolvers/TagResolver";
 
-const startApolloServer = async () => {
-  const schema = await buildSchema({
-    resolvers: [LpResolver, TagResolver],
-    validate: true, 
-  });
-
-  const server = new ApolloServer({ schema });
-
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4001 },
-  });
-  console.log(`🚀  Server ready at: ${url}`);
-};
-
-startApolloServer();
-
+// definition of the datasource 
 const appDataSource = new DataSource({
   type: "sqlite",
   database: "db.sqlite",
@@ -38,201 +21,41 @@ const appDataSource = new DataSource({
   synchronize: true,
 });
 
-const app = express();
+// definition of the port server
+const PORT = 4000;
 
-const port = process.env.REACT_APP_SERVER_PORT;
+// creation of Apollo Server
+const startApolloServer = async () => {
+  // creation of the GraphQL schema that will use Resolvers and class-validator
+  const schema = await buildSchema({
+    resolvers: [LpResolver, TagResolver],
+    validate: true,
+  });
 
-app.listen(port, async () => {
+  // creation of the server that will use theschema
+  const server = new ApolloServer({ schema });
+
+  // creation of the url that the server will use
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: PORT },
+  });
+
+  // datasource initialization
   await appDataSource.initialize();
-  await Category.saveNewCategoryIfNotExisting({ title: "Rap US" });
-  await Category.saveNewCategoryIfNotExisting({ title: "Rap FR" });
-  await Category.saveNewCategoryIfNotExisting({ title: "Classique" });
-  await Category.saveNewCategoryIfNotExisting({ title: "Jazz" });
-  await Category.saveNewCategoryIfNotExisting({ title: "Blues" });
-  await Category.saveNewCategoryIfNotExisting({ title: "Metal" });
-  await Category.saveNewCategoryIfNotExisting({ title: "Soul" });
-  await Category.saveNewCategoryIfNotExisting({ title: "Rock" });
-  await Category.saveNewCategoryIfNotExisting({ title: "Electro" });
-  console.log(`Server is listening on port ${port}`);
-});
 
-app.use(express.json());
+  // list of categories created when datasource is initialized
+  await Category.saveNewCategoryIfNotExisting({ id: 1, title: "Rap US" });
+  await Category.saveNewCategoryIfNotExisting({ id: 2, title: "Rap FR" });
+  await Category.saveNewCategoryIfNotExisting({ id: 3, title: "Classique" });
+  await Category.saveNewCategoryIfNotExisting({ id: 4, title: "Jazz" });
+  await Category.saveNewCategoryIfNotExisting({ id: 5, title: "Blues" });
+  await Category.saveNewCategoryIfNotExisting({ id: 6, title: "Metal" });
+  await Category.saveNewCategoryIfNotExisting({ id: 7, title: "Soul" });
+  await Category.saveNewCategoryIfNotExisting({ id: 8, title: "Rock" });
+  await Category.saveNewCategoryIfNotExisting({ id: 9, title: "Electro" });
 
-app.get("/", (req, res) => {
-  return res.send("HELLO");
-});
+  console.log(`🚀  Server ready at: ${url}`);
+};
 
-app.get("/lps", async (req, res) => {
-  const { query } = req;
-  const lps = await Lp.getAllLps(
-    query.category ? parseInt(query.category as string) : undefined
-  );
-  return res.json({ lps });
-});
-
-app.get("/categories", async (req, res) => {
-  const categories = await Category.getAllCategories();
-  return res.status(200).json({ categories });
-});
-
-app.get("/tags", async (req, res) => {
-  const tags = await Tag.getAllTags();
-  return res.status(200).json({ tags });
-});
-
-app.get("/lps/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const lp = await Lp.getLpById(id);
-    return res.json({ lp });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(404).json({ error: error.message });
-    }
-  }
-});
-
-app.get("/categories/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-
-  try {
-    const category = await Category.getCategoryById(id);
-    return res.status(200).json({ category });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(404).json({ error: error.message });
-    }
-  }
-});
-
-app.get("/tags/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const tag = await Tag.getTagById(id);
-    return res.status(200).json({ tag });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(404).json({ error: error.message });
-    }
-  }
-});
-
-app.delete("/lps/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    await Lp.deleteLp(id);
-    return res.status(204).json({ id });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(404).json({ error: error.message });
-    }
-  }
-});
-
-app.delete("/categories/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-
-  try {
-    await Category.deleteCategory(id);
-    return res.status(204).json({ id });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(404).json({ error: error.message });
-    }
-  }
-});
-
-app.delete("/tags/:id", async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    await Tag.deleteTag(id);
-    return res.status(204).json({ id });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(404).json({ error: error.message });
-    }
-  }
-});
-
-app.post("/lps", async (req, res) => {
-  const lpData = req.body;
-
-  try {
-    const savedLp = await Lp.saveNewLp(lpData);
-    return res.status(201).json({ lp: savedLp });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(400).json({ error: error.message });
-    }
-  }
-});
-
-app.post("/categories", async (req, res) => {
-  const categoryData = req.body;
-
-  const savedCategory = await Category.saveNewCategoryIfNotExisting(
-    categoryData
-  );
-
-  return res.status(201).json({ category: savedCategory });
-});
-
-app.post("/tags", async (req, res) => {
-  const tagData = req.body;
-
-  try {
-    const savedTag = await Tag.saveNewTag(tagData);
-    return res.status(201).json({ tag: savedTag });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(400).json({ error: error.message });
-    }
-  }
-});
-
-app.put("/lps/:id", async (req, res) => {
-  const id = req.params.id;
-
-  const updatedData = req.body;
-
-  try {
-    const updatedLp = await Lp.updateLp(id, updatedData);
-    return res.status(204).json({ lp: updatedLp });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(400).json({ error: error.message });
-    }
-  }
-});
-
-app.put("/categories/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  const updatedData = req.body;
-
-  try {
-    const updatedCategory = await Category.updateCategory(id, updatedData);
-    return res.status(204).json({ category: updatedCategory });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(400).json({ error: error.message });
-    }
-  }
-});
-
-app.put("/tags/:id", async (req, res) => {
-  const id = req.params.id;
-  const updatedData = req.body;
-
-  try {
-    const updatedTag = await Tag.updateTag(id, updatedData);
-    return res.status(204).json({ tag: updatedTag });
-  } catch (error) {
-    if (isError(error)) {
-      return res.status(400).json({ error: error.message });
-    }
-  }
-});
+// Apollo Server initialization
+startApolloServer();
