ea0d22b3dea9e2a5ce5def9534f5c3b02e941c26
VinchC <vinch.cm@gmail.com>
2024-10-09 11:31:19 +0200

    set up Apollo Client

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.css";
+import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import createApolloClient from "./apollo-client";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const client = createApolloClient();
+
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+    {/* Apollo Client should be placed at the top level of the app */}
+      <ApolloProvider client={client}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
     </>
   );
 }
